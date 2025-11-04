4bb9c8a039deb9860c51121284f93ec5cfdfc1dc
ramgopalalavelli <ramgopal.alavelli27@gmail.com>
2025-11-04 17:27:36 +0530

    Redirect home to board page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,6 @@
-'use client';
-
-import LandingHero from './components/LandingHero';
-import LandingCategories from './components/LandingCategories';
-import LandingFeatures from './components/LandingFeatures';
-import LandingPricing from './components/LandingPricing';
+// app/page.tsx
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-gradient-to-b from-white to-emerald-50/40 text-gray-900">
-      {/* Hero */}
-      <LandingHero />
-
-      {/* Categories */}
-      <section className="mt-16">
-        <LandingCategories />
-      </section>
-
-      {/* Features */}
-      <section className="mt-16">
-        <LandingFeatures />
-      </section>
-
-      {/* Pricing */}
-      <section className="mt-20">
-        <LandingPricing />
-      </section>
-    </main>
-  );
+  redirect('/board');
 }
