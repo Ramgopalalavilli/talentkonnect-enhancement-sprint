65d7577ae833a390e7fe92118cd1d427374fd37f
ramgopalalavelli <ramgopal.alavelli27@gmail.com>
2025-11-05 15:56:48 +0530

    fix: normalize Footer casing for Vercel build

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import LandingHero from "./components/LandingHero";
 import CategoryCloud from "./components/CategoryCloud";
 import Steps from "./components/Steps";
 import PricingCTA from "./components/PricingCTA";
-import Footer from "./components/footer";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
